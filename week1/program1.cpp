c2f119c4a998e4b86839e3bb3240f77a811470f0
zamores98 <45889832+zamores98@users.noreply.github.com>
2020-01-23 15:41:25 -0500

    Update program1.cpp

--- week1/program1.cpp
+++ week1/program1.cpp
@@ -1,1 +1,10 @@
-
+/* Program #1 - A first C++ program.
+Enter this program, then compile and run it.
+*/
+#include <iostream>
+using namespace std;
+// main() is where program execution begins.int main()
+{
+  cout << "This is my first C++ program.";
+  return 0;
+}
