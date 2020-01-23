9a319f1425d831f87963ee70fc95a0fef1157b3f
zamores98 <45889832+zamores98@users.noreply.github.com>
2020-01-23 16:02:13 -0500

    Update program2.cpp

--- week1/program2.cpp
+++ week1/program2.cpp
@@ -1,1 +1,17 @@
+// Program #2 - Using a variable
 
+#include <iostream>
+using namespace std;
+
+int main()
+{
+        int x; // this declares a variable
+
+        x = 1023; // this assigns 1023 to x
+
+        cout << "This program prints the value of x; ";
+        cout << x; // This displays 1023
+
+        return 0;
+    
+}
