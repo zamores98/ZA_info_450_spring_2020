b731ec6fbb25ba2069080653f24eb1c917811e0f
zamores98 <45889832+zamores98@users.noreply.github.com>
2020-02-06 18:33:38 -0500

    Update practice1.cpp

--- week2/practice1.cpp
+++ week2/practice1.cpp
@@ -1,16 +1,28 @@
 #include <iostream>
-    #include <cmath>
-    using namespace std;
-    int main(){
-      int input_variable = -1;
-      while (1) {
-        cout << "Please enter a number so I can square it! " << endl;
-        cin >> input_variable;
-        if (input_variable == 0){
-          break;
-        }
-        cout << input_variable << " squared is " << pow(input_variable, 2) << endl;
-      }
-      cout << "You entered 0, I'm out!" << endl;
-      return 0;
+#include <cmath>
+using namespace std;
+int main()
+    {
+    double square, product
+        cout << "Enter a number to square it " << endl;
+
+    cin >> square;    
+    if (square == 0)
+
+    {
+
+    break;
+
     }
+
+    product = square * square
+
+        cout << square << " squared is " << product << endl;
+
+}
+
+cout << "0 ends the program!" << endl;
+
+return 0;
+
+}
