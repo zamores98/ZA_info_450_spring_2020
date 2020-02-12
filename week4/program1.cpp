f69556cbbcdc8ef6fe0fec6e02cd5af3b69dea3e
zamores98 <45889832+zamores98@users.noreply.github.com>
2020-02-12 16:41:03 -0500

    Update program1.cpp

--- week4/program1.cpp
+++ week4/program1.cpp
@@ -1,1 +1,28 @@
-
+    #include <iostream>
+    #include <cmath>
+    using namespace std;
+    float array(float data[]);
+    float mean;
+    float sum;
+    int main()
+    {
+        int x;
+        float data[10];
+        cout << "Please Enter 10 Numbers ";
+        for(x = 0; x < 10; ++x)
+            cin >> data[x];
+        cout << endl << "The STD Deviation of this set is: " << array(data);
+    }
+    float array(float data[])
+    {
+        float sum = 0, mean, stddev = 0;
+        int x;
+        for(x = 0; x < 10; ++x)
+        {
+            sum += data[x];
+        }
+        mean = sum/10;
+        for(x = 0; x < 10; ++x)
+            stddev += pow(data[x] - mean, 2);
+        return sqrt(stddev / 10);
+    }
