22551be18cbc71f4d59ade61f37a038f395719bf
zamores98 <45889832+zamores98@users.noreply.github.com>
2020-03-03 20:02:32 -0500

    Update program1.cpp

--- week7/program1.cpp
+++ week7/program1.cpp
@@ -4,25 +4,24 @@ using namespace std;
 
 int main()
 {
-  string userinput;
-  string again = "Yes";
-  while (again.length() > 0)
-  {
-    
-  cout << "Please enter a string:" << endl;
-  getline(cin,userinput);
-  if (userinput.length() < 1)
-  {
-  again.length() == 0;
-  cout << "Bye";
-  return 0;
-  }
-  
-  else if (userinput.length() > 0)
-  cout << "You entered:" << endl << userinput << endl;
-  cout << "Here is your string in reverse:" << endl;
-  reverse(userinput.begin(),userinput.end());
-  cout << userinput<<endl;
-  }
-  return 0; 
+    string userinput;
+    string again = "Yes";
+    while (again.length() > 0)
+    {
+        cout << "Please enter a string:" << endl;
+        getline(cin,userinput);
+        if (userinput.length() > 0)
+        {
+            cout << "You entered:" << endl << userinput << endl;
+            cout << "Here is your string in reverse:" << endl;
+            reverse(userinput.begin(),userinput.end());
+            cout << userinput<<endl;
+            }
+            else if (userinput.length() < 1)
+            {
+                again.length() == 0;
+                cout << "Bye";
+                return 0;
+        }
+    }
 }
